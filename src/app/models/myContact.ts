c7979684880f17529ba1f35fff319b20d67af657
dipanc1 <dipanchhabra@gmail.com>
2022-06-29 23:18:03 +0530

    first commit

---
+++ src/app/models/myContact.ts
@@ -0,0 +1,10 @@
+export class MyContact{
+  id?: number;
+  name!: string;
+  email!: string;
+  photo!: string;
+  mobile!: string;
+  company!: string;
+  title!: string;
+  groupId!: string;
+}
---
+++ src/app/models/myGroup.ts
@@ -0,0 +1,4 @@
+export class MyGroup{
+  id?: number;
+  name!: string;
+}
